e578ff702eddcfdfe26532930f619d0cb9554348
Ekaterina <80875367+EkaterinaVZ@users.noreply.github.com>
2021-12-23 16:30:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,8 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World!"}
-
+    return {"message": "Hello World"}
 
 @app.post("/predict/")
 def predict(item: Item):
-    """ Text = text in English.
-    Examples: I have to go to the university., There is the house where my family lives., We go jogging every Sunday."""
     return generator(item.text)
-# uvicorn text_generation:app
-# env\Scripts\activate.bat
-
